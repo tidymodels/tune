0d9afff31c3d29892630da06440201fef87a2b07
topepo <mxkuhn@gmail.com>
2019-08-30 13:18:59 -0400

    helper function

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -17,3 +17,17 @@ test_that('determine foreach operator', {
   expect_equal(tune:::get_operator(), foreach::`%do%`)
   expect_equal(tune:::get_operator(FALSE), foreach::`%do%`)
 })
+
+# ------------------------------------------------------------------------------
+
+test_that('exponential decay', {
+  expect_equal(
+    expo_decay(1, start_val = 0, limit_val = 1, slope = 1/5), 0
+  )
+  expect_equal(
+    expo_decay(1000, start_val = 0, limit_val = 1, slope = 1/5), 1
+  )
+  expect_equal(
+    expo_decay(10, start_val = 0, limit_val = 50, slope = 1), (1 - exp(-9)) * 50
+  )
+})
