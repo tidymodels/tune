124aeb07781749764be3e8ee2c46ca5fe3704e0d
topepo <mxkuhn@gmail.com>
2019-09-16 13:00:18 -0400

    looking at resampled vs unresampled statistics

--- inst/examples/Chicago_corr_lm.R
+++ inst/examples/Chicago_corr_lm.R
@@ -1,6 +1,7 @@
 library(tidymodels)
 library(workflows)
 library(tune)
+library(ggforce)
 
 # ------------------------------------------------------------------------------
 
@@ -37,10 +38,42 @@ chi_grid <-
 
 
 ext <- function(x) {
-  tidy(x$model)
+  broom::glance(x$model)
 }
 
 res <- tune_grid(chi_wflow, data_folds, chi_grid, control = grid_control(verbose = TRUE, extract = ext))
+
+# unnest(unnest(res %>% select(id, .extract), cols = .extract), cols = .extract)
+
+lm_stats <-
+  res %>%
+  select(id, .extract) %>%
+  unnest(cols = .extract) %>%
+  unnest(cols = .extract) %>%
+  select(id, threshold, adj.r.squared, sigma, AIC, BIC) %>%
+  group_by(threshold) %>%
+  summarize(
+    adj.r.squared = mean(adj.r.squared, na.rm = TRUE),
+    sigma = mean(sigma, na.rm = TRUE),
+    AIC = mean(AIC, na.rm = TRUE),
+    BIC = mean(BIC, na.rm = TRUE)
+  )
+
+rs_stats <-
+  summarize(res) %>%
+  select(threshold, .metric, mean) %>%
+  pivot_wider(names_from = .metric, values_from = mean, id_cols = threshold)
+
+all_stats <- full_join(lm_stats, rs_stats)
+
+
+
+
+ggplot(all_stats, aes(x = .panel_x, y = .panel_y, colour = threshold)) +
+  geom_point() +
+  facet_matrix(vars(-threshold)) +
+  theme_bw()
+
 
 summarize(res) %>%
   dplyr::filter(.metric == "rmse") %>%
