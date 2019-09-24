fed3bb6f1b566700d4add0e9f1e47ae17d633cb0
topepo <mxkuhn@gmail.com>
2019-09-23 21:39:57 -0400

    examples from Julia's ML class

---
+++ inst/examples/stack_overflow.R
@@ -0,0 +1,88 @@
+library(tidymodels)
+library(tune)
+library(workflows)
+library(readr)
+
+# From https://github.com/juliasilge/supervised-ML-case-studies-course
+# ------------------------------------------------------------------------------
+
+so_train <-
+  read_rds(url("https://github.com/juliasilge/supervised-ML-case-studies-course/blob/master/data/c2_training_full.rds?raw=true")) %>%
+  mutate(Country = as.factor(Country)) %>%
+  mutate_if(is.logical, as.numeric) %>%
+  # ranger doesn't like spaces or "/" and will error with "Illegal column names in
+  # formula interface. Fix column names or use alternative interface in ranger.
+  rename_at(vars(dplyr::contains(" ")), ~ gsub("([[:blank:]])|([[:punct:]])", "_", .))
+
+lr_rec <-
+  recipe(Remote ~ ., data = so_train) %>%
+  step_dummy(Country) %>%
+  step_downsample(Remote) %>%
+  step_zv(all_predictors())
+
+lr_mod <-
+  logistic_reg(penalty = tune(), mixture = tune()) %>%
+  set_engine("glmnet")
+
+rf_rec <-
+  recipe(Remote ~ ., data = so_train) %>%
+  step_downsample(Remote)
+
+rf_mod <-
+  rand_forest(mode = "classification", mtry = tune(), min_n = tune(), trees = 1000) %>%
+  set_engine("ranger")
+
+set.seed(4290)
+so_boots <- bootstraps(so_train)
+
+# ------------------------------------------------------------------------------
+
+library(doParallel)
+cl <- makePSOCKcluster(parallel::detectCores(logical = FALSE))
+registerDoParallel(cl)
+
+# ------------------------------------------------------------------------------
+
+glmn_grid <- expand.grid(penalty = 10^seq(-3, -1, length = 14),
+                         mixture = (0:5)/5)
+
+glmn_search <- tune_grid(lr_rec, lr_mod, rs = so_boots,
+                         grid = glmn_grid,
+                         perf = metric_set(accuracy, roc_auc),
+                         control = grid_control(verbose = TRUE))
+
+summarize(glmn_search) %>%
+  filter(.metric == "accuracy") %>%
+  ggplot(aes(x = penalty, y = mean, col = factor(mixture))) +
+  geom_point() +
+  geom_line() +
+  scale_x_log10()
+
+
+# ------------------------------------------------------------------------------
+
+set.seed(4538)
+rf_grid <-
+  param_set(rf_mod) %>%
+  update("mtry", mtry(c(1, 20))) %>%
+  grid_latin_hypercube(size = 20)
+
+set.seed(1809)
+rf_search <- tune_grid(rf_rec, rf_mod, rs = so_boots,
+                       grid = rf_grid,
+                       perf = metric_set(accuracy, roc_auc))
+
+summarize(rf_search) %>%
+  filter(.metric == "accuracy") %>%
+  select(mtry, min_n, mean) %>%
+  pivot_longer(-mean, names_to = "parameter", values_to = "value") %>%
+  ggplot(aes(x = value, y = mean)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  facet_wrap( ~ parameter, scales = "free_x")
+
+
+summarize(rf_search) %>%
+  filter(.metric == "accuracy") %>%
+  ggplot(aes(x = mtry, y = min_n, size = mean)) +
+  geom_point()
---
+++ inst/examples/voters.R
@@ -0,0 +1,69 @@
+library(tidymodels)
+library(tune)
+library(workflows)
+library(readr)
+
+# From https://github.com/juliasilge/supervised-ML-case-studies-course
+# ------------------------------------------------------------------------------
+
+vote_train <- read_rds(url("https://github.com/juliasilge/supervised-ML-case-studies-course/blob/master/data/c3_training_full.rds?raw=true"))
+
+voters_rec <-
+  recipe(turnout16_2016 ~ ., data = vote_train) %>%
+  step_downsample(turnout16_2016)
+
+lr_mod <-
+  logistic_reg(penalty = tune(), mixture = tune()) %>%
+  set_engine("glmnet")
+
+rf_mod <-
+  rand_forest(mode = "classification", mtry = tune(), min_n = tune(), trees = 1000) %>%
+  set_engine("ranger")
+
+set.seed(6059)
+voter_folds <- vfold_cv(vote_train)
+
+# ------------------------------------------------------------------------------
+
+glmn_grid <- expand.grid(penalty = 10^seq(-3, 0, length = 14),
+                         mixture = (0:5)/5)
+
+glmn_search <- tune_grid(voters_rec, lr_mod, rs = voter_folds,
+                         grid = glmn_grid,
+                         perf = metric_set(accuracy, roc_auc))
+
+summarize(glmn_search) %>%
+  filter(.metric == "accuracy") %>%
+  ggplot(aes(x = penalty, y = mean, col = factor(mixture))) +
+  geom_point() +
+  geom_line() +
+  scale_x_log10()
+
+
+# ------------------------------------------------------------------------------
+
+set.seed(606)
+rf_grid <-
+  param_set(rf_mod) %>%
+  update("mtry", mtry(c(1, 41))) %>%
+  grid_latin_hypercube(size = 20)
+
+set.seed(1354)
+rf_search <- tune_grid(voters_rec, rf_mod, rs = voter_folds,
+                         grid = rf_grid,
+                         perf = metric_set(accuracy, roc_auc))
+
+summarize(rf_search) %>%
+  filter(.metric == "accuracy") %>%
+  select(mtry, min_n, mean) %>%
+  pivot_longer(-mean, names_to = "parameter", values_to = "value") %>%
+  ggplot(aes(x = value, y = mean)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  facet_wrap( ~ parameter, scales = "free_x")
+
+
+summarize(rf_search) %>%
+  filter(.metric == "accuracy") %>%
+  ggplot(aes(x = mtry, y = min_n, size = mean)) +
+  geom_point()
