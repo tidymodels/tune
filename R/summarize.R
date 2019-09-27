715561537c2d4e774cd94c27e97ab319215ec2e6
topepo <mxkuhn@gmail.com>
2019-09-27 10:03:38 -0400

    fixed S3 signature

--- R/summarize.R
+++ R/summarize.R
@@ -1,6 +1,6 @@
 #' Estimate performance values
 #'
-#' @param x An object.
+#' @param .data An object.
 #' @param ... Not currently used.
 #' @return A tibble with parameter values and summaries of their results. A
 #' column for the average (`mean`), the standard error of the mean (`std_err`),
@@ -8,26 +8,26 @@
 #' metric and estimator type.
 #' @export
 #' @rdname summarize.tune_results
-summarise.tune_results <- function(x, ...) {
-  all_bad <- is_cataclysmic(x)
+summarise.tune_results <- function(.data, ...) {
+  all_bad <- is_cataclysmic(.data)
   if (all_bad) {
     stop("All of the models failed.", call. = FALSE)
   }
 
-  tibble_metrics <- purrr::map_lgl(x$.metrics, tibble::is_tibble)
-  x <- x[tibble_metrics, ]
+  tibble_metrics <- purrr::map_lgl(.data$.metrics, tibble::is_tibble)
+  .data <- .data[tibble_metrics, ]
 
-  if (any(names(x) == ".iter")) {
+  if (any(names(.data) == ".iter")) {
     keep_cols <- c(".iter", ".metrics")
   } else {
     keep_cols <- ".metrics"
   }
-  x <- tidyr::unnest(x, cols = dplyr::one_of(keep_cols))
-  all_col <- names(x)
+  .data <- tidyr::unnest(.data, cols = dplyr::one_of(keep_cols))
+  all_col <- names(.data)
   excl_cols <- c(".metric", ".estimator", ".estimate", "splits",
                  grep("^id", all_col, value = TRUE), ".predictions", ".extracts")
   param_names <- all_col[!(all_col %in% excl_cols)]
-  x %>%
+  .data %>%
     tibble::as_tibble() %>%
     dplyr::group_by(!!!rlang::syms(param_names), .metric, .estimator) %>%
     dplyr::summarize(
