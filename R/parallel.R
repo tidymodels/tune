2fa353bb6bbf6b07e9e2de05831f85a9a3438986
topepo <mxkuhn@gmail.com>
2019-09-27 08:06:02 -0400

    only issue warning when 2+ workers

--- R/parallel.R
+++ R/parallel.R
@@ -2,10 +2,10 @@
 # Helpers for parallel processing
 
 get_operator <- function(allow = TRUE, object) {
-
+  is_par <- foreach::getDoParWorkers() > 1
   pkgs <- mod_pkgs(object$fit$model$model)
   blacklist <- c("keras", "rJava")
-  if (allow && any(pkgs %in% blacklist)) {
+  if (is_par & allow && any(pkgs %in% blacklist)) {
     pkgs <- pkgs[pkgs %in% blacklist]
     msg <- paste0("'", pkgs, "'", collapse = ", ")
     msg <- paste("Some required packages prohibit parallel processing: ", msg)
@@ -13,7 +13,7 @@ get_operator <- function(allow = TRUE, object) {
     allow <- FALSE
   }
 
-  cond <- allow && foreach::getDoParWorkers() > 1
+  cond <- allow && is_par
   if (cond) {
     res <- foreach::`%dopar%`
   } else {
