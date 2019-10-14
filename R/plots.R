dfac297be69aa0b81a9e49c5adda06cdb230a1a0
topepo <mxkuhn@gmail.com>
2019-10-14 13:04:27 -0400

    autoplot code for #15

--- R/plots.R
+++ R/plots.R
@@ -1,13 +1,49 @@
-#' Plot sequential search results
+#' Plot search results
 #'
-#' @param x A tibble of results for `tune_Bayes()`.
+#' @param x A tibble of results form `tune_grid()` or `tune_Bayes()`.
+#' @param type A single character value. Choices are `"marginals"` (for a plot
+#' 'of each predictor versus performance), `"parameters"` (each parameter versus
+#' search iteration), or `"performance"` (performance versus iteration). The
+#' latter two choices are only used for `tune_Bayes()`.
 #' @param metric A character vector or `NULL` for which outcome to plot.
-#' @param width A number for the width of the confidence interval bars. A value
-#' of zero prevents them from being shown.
+#' @param width A number for the width of the confidence interval bars when
+#' `type = "perfomance"`. A value of zero prevents them from being shown.
 #' @return A `ggplot2` object.
+#' @details The parameters are currently represented in their natural units.
 #'
+#' A single categorical tuning parameter is supported when other numeric
+#' parameters are also in the results. Any number of numeric tuning parameters
+#' can be used.
 #' @export
-plot_perf_vs_iter <- function(x, metric = NULL, width = max(x$.iter)/75) {
+autoplot.tune_results <-
+  function(object,
+           type = c("marginals", "parameters", "performance"),
+           metric = NULL,
+           width = NULL,
+           ...) {
+    type <- match.arg(type)
+    has_iter <- any(names(object) == ".iter")
+    if (!has_iter && type != "marginals") {
+      rlang::abort(paste0("`type = ", type, "` is only used iterative search results."))
+    }
+
+    if (type == "parameters") {
+      p <- plot_param_vs_iter(object)
+    } else {
+      if (type == "performance") {
+        p <- plot_perf_vs_iter(object, metric, width)
+      } else {
+        p <- plot_marginals(object, metric = metric)
+      }
+    }
+    p
+  }
+
+
+plot_perf_vs_iter <- function(x, metric = NULL, width = NULL) {
+  if (is.null(width)) {
+    width <- max(x$.iter)/75
+  }
   x <- estimate(x)
   if (!is.null(metric)) {
     x <- x %>% dplyr::filter(.metric %in% metric)
@@ -42,9 +78,28 @@ plot_perf_vs_iter <- function(x, metric = NULL, width = max(x$.iter)/75) {
   p
 }
 
+plot_param_vs_iter <- function(x) {
+  x <- estimate(x)
+  excl_cols <- c(".metric", ".estimator", "mean", "n", "std_err", ".iter")
+  param_cols <- names(x)[!((names(x) %in% excl_cols))]
+  is_num <- map_lgl(x %>% dplyr::select(dplyr::one_of(param_cols)), is.numeric)
+  num_param_cols <- param_cols[is_num]
+  x <-
+    x %>%
+    dplyr::select(.iter, dplyr::one_of(num_param_cols)) %>%
+    tidyr::pivot_longer(cols = dplyr::one_of(num_param_cols))
+
+  p <-
+    ggplot(x, aes(x = .iter, y = value)) +
+    geom_point() +
+    xlab("Iteration") +
+    ylab("Parameter Value") +
+    facet_wrap(~ name, scales = "free_y")
+
+  p
+}
 
 plot_marginals <- function(x, metric = NULL) {
-  # maybe add workflow to get transformations, bust up into modules
   x <- estimate(x)
   if (!is.null(metric)) {
     x <- x %>% dplyr::filter(.metric %in% metric)
@@ -69,6 +124,9 @@ plot_marginals <- function(x, metric = NULL) {
     chr_param_cols <- param_cols[!is_num]
     if (length(chr_param_cols) > 1) {
       rlang::abort("Currently cannot autoplot grids with 2+ non-numeric parameters.")
+    }
+    if (length(num_param_cols) == 0) {
+      rlang::abort("Currently cannot autoplot grids with only non-numeric parameters.")
     }
     num_val <- num_val[param_cols %in% chr_param_cols]
     names(num_val) <- chr_param_cols
