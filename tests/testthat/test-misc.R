43a597284ecd6b8a81ee27dc09ba66f186b96c2f
Max Kuhn <mxkuhn@gmail.com>
2019-11-10 14:37:54 -0500

    formula/term generation was inapppropriately using all data

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -32,3 +32,14 @@ test_that('exponential decay', {
     expo_decay(10, start_val = 0, limit_val = 50, slope = 1), (1 - exp(-9)) * 50
   )
 })
+
+
+# ------------------------------------------------------------------------------
+
+test_that('correct size of formula method', {
+  splits <- rsample::vfold_cv(mtcars)
+  form <- list(pre = list(formula_processor = list(formula_processor = mpg ~ .)))
+  res <- tune:::exec_formula(splits$splits[[1]], form)
+  expect_equivalent(nrow(res$x), dim(splits$splits[[1]])[1])
+  expect_equivalent(nrow(res$y), dim(splits$splits[[1]])[1])
+})
