7f1519c50a61884eee08e519f85a0bc6a838ca82
DavisVaughan <davis@rstudio.com>
2019-11-12 15:04:50 -0500

    Add custom `tune_symbol` for better unicode support

---
+++ R/zzz.R
@@ -0,0 +1,30 @@
+.onLoad <- function(libname, pkgname) {
+  ns <- rlang::ns_env("tune")
+
+  # Modified version of the cli .onLoad()
+  # We can't use cli::symbol$tick because the width of the character
+  # looks awful when you output it alongside info / warning characters
+  makeActiveBinding(
+    "tune_symbol",
+    function() {
+      # If `cli.unicode` is set we use that
+      opt <- getOption("cli.unicode",  NULL)
+
+      if (!is.null(opt)) {
+        if (isTRUE(opt)) return(tune_symbol_utf8) else return(tune_symbol_ascii)
+      }
+
+      # Otherwise we try to auto-detect
+      if (cli::is_utf8_output()) {
+        tune_symbol_utf8
+      } else if (is_latex_output()) {
+        tune_symbol_ascii
+      } else if (is_windows()) {
+        tune_symbol_windows
+      } else {
+        tune_symbol_ascii
+      }
+    },
+    ns
+  )
+}
