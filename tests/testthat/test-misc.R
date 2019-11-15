e91f4a656e171f5ac07b092ce32fa42b3b84f1ee
DavisVaughan <davis@rstudio.com>
2019-11-15 14:48:13 -0500

    Remove dead code

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -32,14 +32,3 @@ test_that('exponential decay', {
     expo_decay(10, start_val = 0, limit_val = 50, slope = 1), (1 - exp(-9)) * 50
   )
 })
-
-
-# ------------------------------------------------------------------------------
-
-test_that('correct size of formula method', {
-  splits <- rsample::vfold_cv(mtcars)
-  form <- list(pre = list(actions = list(formula = list(formula = mpg ~ .))))
-  res <- tune:::exec_formula(splits$splits[[1]], form)
-  expect_equivalent(nrow(res$x), dim(splits$splits[[1]])[1])
-  expect_equivalent(nrow(res$y), dim(splits$splits[[1]])[1])
-})
