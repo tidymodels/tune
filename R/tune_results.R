83ddb0f8e34a9eb861cb1e9feef84eddaba3f436
Julia Silge <julia.silge@gmail.com>
2020-03-23 18:01:30 -0600

    Namespace the glue function

--- R/tune_results.R
+++ R/tune_results.R
@@ -2,17 +2,17 @@
 print.tune_results <- function(x, ...) {
   total_notes <- sum(map_int(x$.notes, nrow)) > 0
   if (total_notes > 0) {
-    print_notes <- glue("This tuning result has notes.\n",
-                        "Example notes on model fitting include:\n",
-                        glue::glue_collapse(
-                          x %>%
-                            dplyr::select(.notes) %>%
-                            tidyr::unnest(.notes) %>%
-                            dplyr::mutate(note = dplyr::row_number()) %>%
-                            dplyr::sample_n(3) %>%
-                            dplyr::arrange(note) %>%
-                            dplyr::pull(.notes),
-                          sep = "\n"))
+    print_notes <- glue::glue("This tuning result has notes.\n",
+                              "Example notes on model fitting include:\n",
+                              glue::glue_collapse(
+                                x %>%
+                                  dplyr::select(.notes) %>%
+                                  tidyr::unnest(.notes) %>%
+                                  dplyr::mutate(note = dplyr::row_number()) %>%
+                                  dplyr::sample_n(3) %>%
+                                  dplyr::arrange(note) %>%
+                                  dplyr::pull(.notes),
+                                sep = "\n"))
     warning(print_notes, call. = FALSE)
   }
   class(x) <- class(x)[!(class(x) %in% c("tune_results"))]
