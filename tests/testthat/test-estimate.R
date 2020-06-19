e8acbb65c7ad5b01d17493704633f182aba122a3
Max Kuhn <mxkuhn@gmail.com>
2020-06-18 20:30:36 -0400

    removed .groups in tests

--- tests/testthat/test-estimate.R
+++ tests/testthat/test-estimate.R
@@ -7,15 +7,18 @@ rcv_results <- readRDS(test_path("rcv_results.rds"))
 
 # ------------------------------------------------------------------------------
 
+opt <- getOption("dplyr.summarise.inform", default = "FALSE")
+options(dplyr.summarise.inform = FALSE)
+
 compl <-
   unnest(rcv_results, .metrics) %>%
   group_by(deg_free, degree, `wt df`, `wt degree`, .config, .metric, .estimator) %>%
   summarize(mean = mean(.estimate, na.rm = TRUE), n = sum(!is.na(.estimator)),
-            std_err = sd(.estimate, na.rm = TRUE)/sqrt(n),
-            .groups = "keep") %>%
+            std_err = sd(.estimate, na.rm = TRUE)/sqrt(n)) %>%
   ungroup() %>%
   arrange(.config)
 
+options(dplyr.summarise.inform = opt)
 
 test_that('estimate method', {
   expect_equivalent(
