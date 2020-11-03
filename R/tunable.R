74489b6abe56219842b754272f4f0315a1ccce1a
Julia Silge <julia.silge@gmail.com>
2020-11-02 17:03:07 -0700

    Remove tunable.recipe (moved to recipes)

--- R/tunable.R
+++ R/tunable.R
@@ -2,17 +2,17 @@
 #'
 #' [tunable()] determines which parameters in an object _can_ be tuned along
 #' with information about the parameters.
-#' @param x An object, such as a recipe, recipe step, or `parsnip` model
+#' @param x An object, such as a workflow or `parsnip` model
 #' specification.
 #' @param ... Not currently used.
 #' @return A tibble with a column for the parameter `name`, information on the
 #'  _default_ method for generating a corresponding parameter object, the
-#'  `source` of the parameter (e.g. "recipe", etc.), and the `component` within
-#'  the source. For the `component` column, a little more specificity is given
-#'  about the location of the parameter (e.g. "step_normalize" or recipes or
-#'  "boost_tree" for models). The `component_id` column contains the unique step
-#'  `id` field or, for models, a logical for whether the model specification
-#'  argument was a main parameter or one associated with the engine.
+#'  `source` of the parameter (e.g. "model_spec", etc.), and the `component`
+#'  within the source. For the `component` column, a little more specificity is
+#'  given about the location of the parameter (e.g. "boost_tree" for models).
+#'  The `component_id` column contains the unique step `id` field or, for
+#'  models, a logical for whether the model specification argument was a main
+#'  parameter or one associated with the engine.
 #' @details
 #' For a model specification, an engine must be chosen.
 #'
@@ -21,22 +21,11 @@
 #' The information about the default parameter object takes the form of a
 #' named list with an element for the function call and an optional element for
 #' the source of the function (e.g. the `dials` package). For model
-#' specifications, If the parameter is unknown to the underlying `tunable`
+#' specifications, if the parameter is unknown to the underlying `tunable`
 #' method, a `NULL` is returned.
 #' @keywords internal
 #' @examples
 #' \donttest{
-#' library(recipes)
-#'
-#' recipe(mpg ~ ., data = mtcars) %>%
-#'   step_knnimpute(all_predictors()) %>%
-#'   step_pca(all_predictors()) %>%
-#'   tunable()
-#'
-#' recipe(mpg ~ ., data = mtcars) %>%
-#'   step_normalize(all_predictors()) %>%
-#'   tunable()
-#'
 #' library(parsnip)
 #'
 #' boost_tree() %>%
@@ -87,6 +76,8 @@ tunable.model_spec <- function(x, ...) {
   arg_vals %>% dplyr::select(name, call_info, source, component, component_id)
 }
 
+mod_type <- function(.mod) class(.mod)[class(.mod) != "model_spec"][1]
+
 # ------------------------------------------------------------------------------
 
 #' @export
@@ -104,38 +95,6 @@ tunable.workflow <- function(x, ...) {
 
   param_data
 }
-
-# ------------------------------------------------------------------------------
-
-#' @rdname tunable
-#' @export
-tunable.recipe <- function(x, ...) {
-  if (length(x$steps) == 0) {
-    res <- no_param
-  } else {
-    res <- purrr::map_dfr(x$steps, tunable)
-    if (nrow(res) > 0) {
-      res <- res[!is.na(res$name),]
-    }
-  }
-  res
-}
-
-# ------------------------------------------------------------------------------
-
-#' @rdname tunable
-#' @export
-no_param <-
-  tibble::tibble(
-    name = NA_character_,
-    call_info = list(),
-    source = NA_character_,
-    component = NA_character_,
-    component_id = NA_character_
-  )
-
-step_type <- function(.step) class(.step)[class(.step) != "step"][1]
-mod_type <- function(.mod) class(.mod)[class(.mod) != "model_spec"][1]
 
 # ------------------------------------------------------------------------------
 
