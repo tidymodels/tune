06c3a2ec9bb036e3c83e9cb976ec0f6ca392d9ba
Hannah Frick <hannah@rstudio.com>
2022-02-08 09:39:09 +0000

    reduce the tests
    the `extract_parameter_set_dials()` methods get tested in their respective pkgs

--- tests/testthat/test-param_set.R
+++ tests/testthat/test-param_set.R
@@ -6,73 +6,21 @@ source(test_path("../helper-objects.R"))
 
 # ------------------------------------------------------------------------------
 
-test_that('recipe with no steps', {
-  withr::local_options(lifecycle_verbosity = "quiet")
-
-  bare_info <- dials::parameters(bare_rec)
-  check_param_set_tibble(bare_info)
-  expect_equal(nrow(bare_info), 0)
-})
-
-test_that('recipe with no tunable parameters', {
-  withr::local_options(lifecycle_verbosity = "quiet")
-
-  rm_info <- dials::parameters(rm_rec)
-  check_param_set_tibble(rm_info)
-  expect_equal(nrow(rm_info), 0)
-})
-
-test_that('recipe with tunable parameters', {
+test_that('parameters.recipe() still works', {
   withr::local_options(lifecycle_verbosity = "quiet")
 
   spline_info <- dials::parameters(spline_rec)
   check_param_set_tibble(spline_info)
-  expected_cols <- c('step_impute_knn', 'step_other', 'step_bs', 'step_bs')
-  expect_equal(
-    spline_info$component,
-    expected_cols,
-  )
-  expect_true(all(spline_info$source == "recipe"))
-  nms <- c('neighbors', 'threshold', 'deg_free', 'degree')
-  expect_equal(spline_info$name, nms)
-  ids <- c('imputation', 'threshold', 'deg_free', 'degree')
-  expect_equal(spline_info$id, ids)
-
-  expect_equal(spline_info$object[[1]], dials::neighbors(c(1, 10)))
-  expect_equal(spline_info$object[[2]], dials::threshold(c(0, 1/10)))
-  expect_equal(spline_info$object[[3]], dials::spline_degree(c(1, 15)))
-  expect_equal(spline_info$object[[4]], dials::degree_int(c(1, 2)))
-
 })
 
 # ------------------------------------------------------------------------------
 
-test_that('model with no parameters', {
-  withr::local_options(lifecycle_verbosity = "quiet")
-
-  skip_if_not_installed("parsnip")
-  lm_info <- dials::parameters(lm_model)
-  check_param_set_tibble(lm_info)
-  expect_equal(nrow(lm_info), 0)
-})
-
-test_that('model with main and engine parameters', {
+test_that('parameters.model_spec() still works', {
   withr::local_options(lifecycle_verbosity = "quiet")
 
   skip_if_not_installed("parsnip")
   c5_info <- dials::parameters(bst_model)
   check_param_set_tibble(c5_info)
-  expect_equal(nrow(c5_info), 2)
-  expect_true(all(c5_info$source == "model_spec"))
-  expect_true(all(c5_info$component == "boost_tree"))
-  expect_equal(c5_info$component_id, c("main", "engine"))
-  nms <- c("trees", "rules")
-  expect_equal(c5_info$name, nms)
-  ids <- c("funky name \n", "rules")
-  expect_equal(c5_info$id, ids)
-
-  expect_equal(c5_info$object[[1]], dials::trees(c(1, 100)))
-  expect_equal(c5_info$object[[2]], NA)
 })
 
 
