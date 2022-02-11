5e699f716e8f2d8e69bbf9dbac47a6fdccd06edd
Hannah Frick <hannah@rstudio.com>
2022-02-11 14:28:59 +0000

    make it less brittle
    don't test against sth from another pkg

--- tests/testthat/test-filter-parameters.R
+++ tests/testthat/test-filter-parameters.R
@@ -82,8 +82,7 @@ test_that("bad inputs",{
     "There are no columns in"
   )
   expect_error(
-    filter_parameters(svm_reg_results, tibble(soup = 1)),
-    "must be a logical"
+    filter_parameters(svm_reg_results, tibble(soup = 1))
   )
   expect_error(
     filter_parameters(svm_reg_results, parameters = tibble(`%^*#` = 1/3)),
