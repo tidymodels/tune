ac8a7d1a5550c25cc0ba9e46ed431b8a8d48b304
Hannah Frick <hannah@rstudio.com>
2022-03-04 15:02:09 +0000

    update `test-filter-parameters.R`

--- tests/testthat/test-filter-parameters.R
+++ tests/testthat/test-filter-parameters.R
@@ -3,30 +3,8 @@ load(test_path("test_objects.RData"))
 
 # ------------------------------------------------------------------------------
 
-rec_tune_1 <-
-  recipe(mpg ~ ., data = mtcars) %>%
-  step_normalize(all_predictors()) %>%
-  step_pca(all_predictors(), num_comp = tune())
-
-lm_mod <- linear_reg() %>% set_engine("lm")
-
-set.seed(363)
-mt_folds <- rsample::vfold_cv(mtcars, v = 3)
-
-extr_rec <- function(x) {
-  extract_recipe(x) %>% tidy(number = 2)
-}
-lm_res <-
-  workflow() %>%
-  add_recipe(rec_tune_1) %>%
-  add_model(lm_mod) %>%
-  tune_grid(resamples = mt_folds, control = control_grid(extract = extr_rec),
-            grid = tibble(num_comp = 1:2))
-
-# ------------------------------------------------------------------------------
-
 test_that("basic functionality",{
-  filtered_grid <- filter_parameters(svm_reg_results, parameters = tibble(`%^*#` = 1))
+  filtered_grid <- filter_parameters(svm_reg_results, parameters = tibble::tibble(`%^*#` = 1))
   expect_true(all(purrr::map_lgl(filtered_grid$.metrics, ~ all(.x$`%^*#` == 1))))
   expect_true(all(purrr::map_lgl(filtered_grid$.predictions, ~ all(.x$`%^*#` == 1))))
 
@@ -39,7 +17,7 @@ test_that("basic functionality",{
   expect_true(all(purrr::map_lgl(best_grid$.predictions, ~ all(.x$`%^*#` == best_param$`%^*#`))))
   expect_true(all(purrr::map_lgl(best_grid$.predictions, ~ all(.x$scale_factor == best_param$scale_factor))))
 
-  filtered_grid <- filter_parameters(mt_knn_bo, parameters = tibble(neighbors = 1))
+  filtered_grid <- filter_parameters(mt_knn_bo, parameters = tibble::tibble(neighbors = 1))
   expect_true(all(purrr::map_lgl(filtered_grid$.metrics, ~ all(.x$neighbors == 1))))
   expect_true(all(purrr::map_lgl(filtered_grid$.predictions, ~ all(.x$neighbors == 1))))
 
@@ -48,7 +26,27 @@ test_that("basic functionality",{
   expect_true(all(purrr::map_lgl(best_grid$.metrics, ~ all(.x$neighbors == best_param$neighbors))))
   expect_true(all(purrr::map_lgl(best_grid$.predictions, ~ all(.x$neighbors == best_param$neighbors))))
 
-  filtered_grid <- filter_parameters(lm_res, parameters = tibble(num_comp = 1))
+  rec_tune_1 <-
+    recipes::recipe(mpg ~ ., data = mtcars) %>%
+    recipes::step_normalize(recipes::all_predictors()) %>%
+    recipes::step_pca(recipes::all_predictors(), num_comp = tune())
+
+  lm_mod <- parsnip::linear_reg() %>% parsnip::set_engine("lm")
+
+  set.seed(363)
+  mt_folds <- rsample::vfold_cv(mtcars, v = 3)
+
+  extr_rec <- function(x) {
+    extract_recipe(x) %>% tidy(number = 2)
+  }
+  lm_res <-
+    workflow() %>%
+    add_recipe(rec_tune_1) %>%
+    add_model(lm_mod) %>%
+    tune_grid(resamples = mt_folds, control = control_grid(extract = extr_rec),
+              grid = tibble::tibble(num_comp = 1:2))
+
+  filtered_grid <- filter_parameters(lm_res, parameters = tibble::tibble(num_comp = 1))
   expect_true(all(purrr::map_lgl(filtered_grid$.metrics, ~ all(.x$num_comp == 1))))
   expect_true(all(purrr::map_lgl(filtered_grid$.extracts, ~ all(.x$num_comp == 1))))
 
@@ -68,23 +66,19 @@ test_that("basic functionality",{
 test_that("bad inputs",{
   skip_if(tune:::dplyr_pre_1.0.0())
 
-  expect_error(
-    filter_parameters(collect_metrics(svm_reg_results), parameters = tibble(`%^*#` = 1)),
-    "should have class 'tune_results'"
+  expect_snapshot(error = TRUE,
+    filter_parameters(collect_metrics(svm_reg_results), parameters = tibble::tibble(`%^*#` = 1))
     )
-  expect_error(
-    filter_parameters(svm_reg_results, parameters = tibble(soup = 1)),
-    "There are no columns in"
+  expect_snapshot(error = TRUE,
+    filter_parameters(svm_reg_results, parameters = tibble::tibble(soup = 1))
   )
-  expect_error(
-    filter_parameters(svm_reg_results, tibble(soup = 1))
+  expect_snapshot(error = TRUE,
+    filter_parameters(svm_reg_results, tibble::tibble(soup = 1))
   )
-  expect_error(
-    filter_parameters(svm_reg_results, parameters = tibble(`%^*#` = 1/3)),
-    "No parameter combinations were selected"
+  expect_snapshot(error = TRUE,
+    filter_parameters(svm_reg_results, parameters = tibble::tibble(`%^*#` = 1/3))
   )
-  expect_warning(
-    filter_parameters(svm_reg_results, parameters = tibble(`%^*#` = 1, soup = 2)),
-    "There are unneeded columns in `parameters`"
+  expect_snapshot(
+    filter_parameters(svm_reg_results, parameters = tibble::tibble(`%^*#` = 1, soup = 2))
   )
  })
