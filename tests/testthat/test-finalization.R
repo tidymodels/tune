a0e7632a4f42a70215d467830ac1a9e5d0138810
Hannah Frick <hannah@rstudio.com>
2022-03-04 15:07:32 +0000

    update `test-finalization.R`

--- tests/testthat/test-finalization.R
+++ tests/testthat/test-finalization.R
@@ -1,40 +1,48 @@
-set.seed(21983)
-rs <- rsample::vfold_cv(mtcars)
-
-mod_1 <-
-  parsnip::rand_forest(mtry = tune(), trees = 20, min_n = tune()) %>%
-  parsnip::set_engine("randomForest") %>%
-  parsnip::set_mode("regression")
-
-rec_1 <-
-  recipes::recipe(mpg ~ ., data = mtcars) %>%
-  recipes::step_ns(disp, deg_free = tune())
-
-rec_2 <-
-  recipes::recipe(mpg ~ ., data = mtcars) %>%
-  recipes::step_ns(disp, deg_free = 3)
-
-## -----------------------------------------------------------------------------
-
 test_that('cannot finalize with recipe parameters', {
   skip_if_not_installed("randomForest")
 
-  expect_error(
-    mod_1 %>% tune_grid(rec_1, resamples = rs, grid = 3),
-    "Some tuning parameters require finalization"
+  set.seed(21983)
+  rs <- rsample::vfold_cv(mtcars)
+
+  mod_1 <-
+    parsnip::rand_forest(mtry = tune(), trees = 20, min_n = tune()) %>%
+    parsnip::set_engine("randomForest") %>%
+    parsnip::set_mode("regression")
+
+  rec_1 <-
+    recipes::recipe(mpg ~ ., data = mtcars) %>%
+    recipes::step_ns(disp, deg_free = tune())
+
+  rec_2 <-
+    recipes::recipe(mpg ~ ., data = mtcars) %>%
+    recipes::step_ns(disp, deg_free = 3)
+
+  expect_snapshot(error = TRUE,
+    mod_1 %>% tune_grid(rec_1, resamples = rs, grid = 3)
   )
 
   set.seed(987323)
   expect_error(
-    mod_1 %>% tune_grid(rec_2, resamples = rs, grid = 3),
+    suppressMessages(mod_1 %>% tune_grid(rec_2, resamples = rs, grid = 3)),
     regex = NA
   )
-
 })
 
 
 test_that('skip error if grid is supplied', {
   skip_if_not_installed("randomForest")
+
+  set.seed(21983)
+  rs <- rsample::vfold_cv(mtcars)
+
+  mod_1 <-
+    parsnip::rand_forest(mtry = tune(), trees = 20, min_n = tune()) %>%
+    parsnip::set_engine("randomForest") %>%
+    parsnip::set_mode("regression")
+
+  rec_1 <-
+    recipes::recipe(mpg ~ ., data = mtcars) %>%
+    recipes::step_ns(disp, deg_free = tune())
 
   grid <- tibble::tibble(mtry = 1:3, deg_free = c(3, 3, 4), min_n = c(5,4,6))
 
