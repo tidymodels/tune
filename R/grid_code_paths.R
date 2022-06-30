feedbb97c344d02f54f8fcb3eaff0116070a03e6
qiushi <qiushi.yann@gmail.com>
2022-06-29 21:44:31 -0500

    update error message

--- R/grid_code_paths.R
+++ R/grid_code_paths.R
@@ -86,8 +86,12 @@ tune_grid_loop_agua <- function(resamples,
     rlang::abort("`agua` must be installed to use an h2o parsnip engine.")
   }
 
-  if (!is.null(grid) && !is_regular_grid(grid)) {
-    rlang::abort("The h2o engine only supports regular tuning grids.")
+  if (!is_regular_grid(grid)) {
+    msg <- paste0(
+        "The h2o engine only supports regular tuning grids. ",
+        "Set `grid` explicitly to be a regular grid data frame."
+    )
+    rlang::abort(msg)
   }
 
   parallel_over <- control$parallel_over
