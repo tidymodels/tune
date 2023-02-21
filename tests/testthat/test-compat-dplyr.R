ab597fc7146b5ca073231c743c6af2bf4c08ae31
simonpcouch <simonpatrickcouch@gmail.com>
2023-02-21 14:11:43 -0500

    address join warnings in dplyr compatibility tests

--- tests/testthat/test-compat-dplyr.R
+++ tests/testthat/test-compat-dplyr.R
@@ -168,7 +168,7 @@ test_that("left_join() can keep tune_results class if tune_results structure is 
 test_that("left_join() can lose tune_results class if rows are added", {
   for (x in helper_tune_results) {
     y <- tibble(id = x$id[[1]], x = 1:2)
-    expect_s3_class_bare_tibble(left_join(x, y, by = "id"))
+    expect_s3_class_bare_tibble(left_join(x, y, by = "id", multiple = "all"))
   }
 })
 
@@ -195,7 +195,7 @@ test_that("right_join() can keep tune_results class if tune_results structure is
 test_that("right_join() can lose tune_results class if rows are added", {
   for (x in helper_tune_results) {
     y <- tibble(id = x$id[[1]], x = 1:2)
-    expect_s3_class_bare_tibble(right_join(x, y, by = "id"))
+    expect_s3_class_bare_tibble(right_join(x, y, by = "id", multiple = "all"))
   }
 })
 
@@ -203,7 +203,7 @@ test_that("right_join() restores to the type of first input", {
   for (x in helper_tune_results) {
     y <- tibble(id = x$id[[1]], x = 1)
     # technically tune_results structure is intact, but `y` is a bare tibble!
-    expect_s3_class_bare_tibble(right_join(y, x, by = "id"))
+    expect_s3_class_bare_tibble(right_join(y, x, by = "id", multiple = "all"))
   }
 })
 
