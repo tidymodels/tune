4c1dc7e5eb64898e46488dc5e36e63078a3dd425
Max Kuhn <mxkuhn@gmail.com>
2023-08-23 10:27:45 -0400

    function definition

---
+++ R/bootstrap_623.R
@@ -0,0 +1,56 @@
+#' Bootstrap 632 estimate of performance
+#'
+#' This method blends the bootstrap estimate with the resubsitution estimate
+#' to potentially reduce bias.
+#'
+#' @param x An object of class `'tune_results'` that used bootstrap sampling
+#' with the option `bootstraps(apparent = TRUE)`.
+#' @return A tibble with columns that emulate the structure of
+#' `collect_metrics(summarize = TRUE)`. The `std_err` column is set to missing
+#' values since the standard error of the 632 estimator is unknown.
+#' @export
+bootstrap_623 <- function(x) {
+  if (!inherits(x, "tune_results")) {
+    rlang::abort("The object must have class 'tune_results'.")
+
+  }
+  rs_type <- attributes(x)$rset_info$att$class[1]
+  if (rs_type != "bootstraps") {
+    rlang::abort("The methods is only applicable for bootstrap sampling.")
+  }
+  if (!any(x$id == "Apparent")) {
+    rlang::abort("The option `apparent = TRUE` to `bootstraps()` should be set.")
+  }
+
+  mts <- collect_metrics(x, summarize = FALSE)
+
+  resub <-
+    dplyr::filter(mts, id == "Apparent") %>%
+    dplyr::select(.metric, .config, resub = .estimate)
+
+  bts <-
+    dplyr::filter(mts, id != "Apparent") %>%
+    dplyr::summarize(
+      mean = mean(.estimate, na.rm = TRUE),
+      n = sum(!is.na(.estimate)),
+      .by = c(.metric, .config))
+
+  prm <- .get_tune_parameter_names(x)
+  bys <- c(prm, ".config", ".estimator")
+  key <-
+    collect_metrics(x, summarize = TRUE) %>%
+    dplyr::select(dplyr::all_of(bys)) %>%
+    dplyr::distinct()
+
+  cn <- exp(-1)
+  res <-
+    dplyr::right_join(bts, resub, by = c(".metric", ".config")) %>%
+    dplyr::mutate(
+      mean = (cn * resub) + ((1 - cn) * mean),
+      std_err = NA_real_
+    ) %>%
+    dplyr::right_join(key, by = ".config")
+
+  nms <- c(prm, ".metric", ".estimator", "mean", "n", "std_err", ".config")
+  res[, nms]
+}
