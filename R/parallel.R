e79aac126dbe6855f4508f4e9941f5748d5c815b
simonpcouch <simonpatrickcouch@gmail.com>
2024-03-11 10:09:12 -0500

    remove foreach deprecation warning
    To be re-introduced once these changes have made it to CRAN, for the follow-up release.

--- R/parallel.R
+++ R/parallel.R
@@ -27,36 +27,9 @@ get_operator <- function(allow = TRUE, object) {
       list(op = foreach::`%dopar%`, is_future = FALSE),
       list(op = doFuture::`%dofuture%`, is_future = TRUE)
     )
-
-    if (!res[["is_future"]]) {
-      cli::cli_warn(c(
-        "!" = "{.pkg tune} detected a parallel backend registered with \\
-               foreach but no backend registered with future.",
-        "i" = "Support for foreach was soft-deprecated in {.pkg tune} 1.2.0.",
-        "i" = "See {.help [?parallelism](tune::parallelism)} to learn more."
-      ))
-    }
   } else {
     res <- list(op = foreach::`%do%`, is_future = FALSE)
   }
 
   res
 }
-
-#' Support for parallel processing in tune
-#'
-#' @description
-#' Support for parallel backends registered with the [foreach][foreach::foreach]
-#' package was deprecated in tune 1.2.0 in favor of the
-#' [future][future::future] package. tune 1.2.0 was released in March 2024; for
-#' the two years following, the package will raise a warning when:
-#'
-#' 1) A parallel backend has been registered with foreach, and
-#' 2) No [plan][future::plan] has been specified with future.
-#'
-#' If parallelism has been configured with both framework, tune will use the
-#' plan specified with future and will not warn. To learn more about plans in
-#' future, see [future::plan()].
-#'
-#' @name parallelism
-NULL
