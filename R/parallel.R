627d0e69fedbf2f1cc2dd4ad21c4449bdf36991c
simonpcouch <simonpatrickcouch@gmail.com>
2024-03-22 09:14:25 -0500

    warn when parallel processing with foreach

--- R/parallel.R
+++ R/parallel.R
@@ -27,9 +27,50 @@ get_operator <- function(allow = TRUE, object) {
       list(op = foreach::`%dopar%`, is_future = FALSE),
       list(op = doFuture::`%dofuture%`, is_future = TRUE)
     )
+
+    if (!res[["is_future"]]) {
+      cli::cli_warn(c(
+        "!" = "{.pkg tune} detected a parallel backend registered with \\
+               foreach but no backend registered with future.",
+        "i" = "Support for parallel processing with foreach was \\
+               soft-deprecated in {.pkg tune} 1.2.1.",
+        "i" = "See {.help [?parallelism](tune::parallelism)} to learn more."
+      ))
+    }
   } else {
     res <- list(op = foreach::`%do%`, is_future = FALSE)
   }
 
   res
 }
+
+#' Support for parallel processing in tune
+#'
+#' @description
+#' Support for parallel backends registered with the [foreach][foreach::foreach]
+#' package was deprecated in tune 1.2.1 in favor of the
+#' [future][future::future] package. The package will now raise a warning when:
+#'
+#' 1) A parallel backend has been registered with foreach, and
+#' 2) No [plan][future::plan] has been specified with future.
+#'
+#' If parallelism has been configured with both framework, tune will use the
+#' plan specified with future and will not warn. To transition your code from
+#' foreach to future, remove code that registers a foreach `Backend`:
+#'
+#' ```r
+#' library(doBackend)
+#' registerDoBackend(cores = 4)
+#' ```
+#'
+#' And replace it with:
+#'
+#' ```r
+#' library(future)
+#' plan(multisession, workers = 4)
+#' ```
+#'
+#' See [future::plan()] for possible options other than `multisession`.
+#'
+#' @name parallelism
+NULL
