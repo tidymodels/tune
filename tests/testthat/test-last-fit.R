7fda326cf901b5cb719dd3a65a2f7b5416b042da
‘topepo’ <mxkuhn@gmail.com>
2024-12-11 15:17:13 -0500

    update other tests for dev versions

--- tests/testthat/test-last-fit.R
+++ tests/testthat/test-last-fit.R
@@ -234,7 +234,7 @@ test_that("last_fit() can include validation set for initial_validation_split ob
 test_that("can use `last_fit()` with a workflow - postprocessor (requires training)", {
   skip_if_not_installed("tailor")
   skip_if_not_installed("mgcv")
-  skip_if_not_installed("tailor", minimum_version = "0.0.0.9002")
+  skip_if_not_installed("tailor", minimum_version = "0.0.0.9001")
 
   y <- seq(0, 7, .001)
   dat <- data.frame(y = y, x = y + (y-3)^2)
@@ -278,7 +278,7 @@ test_that("can use `last_fit()` with a workflow - postprocessor (requires traini
 })
 
 test_that("can use `last_fit()` with a workflow - postprocessor (does not require training)", {
-  skip_if_not_installed("tailor", minimum_version = "0.0.0.9002")
+  skip_if_not_installed("tailor", minimum_version = "0.0.0.9001")
 
   y <- seq(0, 7, .001)
   dat <- data.frame(y = y, x = y + (y-3)^2)
