597e36fcaa7dd4ce6aaf7be906ddb73537b0005b
‘topepo’ <mxkuhn@gmail.com>
2025-02-21 10:43:23 -0500

    updates from #981

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,7 @@
 .onLoad <- function(libname, pkgname) {
   ns <- rlang::ns_env("tune")
 
+  ns$manange_global_limit()
 
   # Modified version of the cli .onLoad()
   # We can't use cli::symbol$tick because the width of the character
