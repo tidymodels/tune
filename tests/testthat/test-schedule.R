10791b3afe2af012c9fd4e626805e1c48463630a
Hannah Frick <hannah@posit.co>
2025-02-26 18:22:28 +0000

    reorder

--- tests/testthat/test-schedule.R
+++ tests/testthat/test-schedule.R
@@ -1,4 +1,65 @@
 
+# `get_param_info()` -----------------------------------------------------
+
+test_that("`get_param_info()` works for a workflow without tags for tuning", {
+	wflow <- workflow(mpg ~ ., parsnip::linear_reg())
+	param_info <- get_param_info(wflow)
+
+	expect_named(param_info, c("name", "id", "source", "has_submodel"))
+	expect_identical(nrow(param_info), 0L)
+})
+
+test_that("`get_param_info()` works for a workflow with tags for tuning", {
+	# tuning tags in all components
+	rec_tune_2 <- recipes::recipe(mpg ~ ., data = mtcars) %>%
+		recipes::step_corr(recipes::all_predictors(), threshold = tune()) %>%
+		recipes::step_spline_natural(disp, deg_free = tune("disp_df"))
+
+	mod_tune_no_submodel <- parsnip::rand_forest(
+		min_n = tune(),
+		mode = "regression"
+	)
+
+	tlr_tune <- tailor::tailor() %>%
+		tailor::adjust_numeric_range(lower_limit = tune())
+
+	wflow <- workflow(rec_tune_2, mod_tune_no_submodel, tlr_tune)
+
+	param_info <- get_param_info(wflow)
+
+	expect_named(param_info, c("name", "id", "source", "has_submodel"))
+	expect_identical(
+		param_info$id,
+		c("min_n", "threshold", "disp_df", "lower_limit")
+	)
+	expect_identical(param_info$has_submodel, c(FALSE, NA, NA, NA))
+})
+
+test_that("`get_param_info()` works when there are submodel parameters", {
+	# tuning tags only in model spec
+	rec_no_tuning <- recipes::recipe(mpg ~ ., data = mtcars)
+
+	mod_tune_submodel <- parsnip::boost_tree(
+		trees = tune(),
+		min_n = tune(),
+		mode = "regression"
+	)
+
+	tlr_no_tune <- tailor::tailor() %>%
+		tailor::adjust_numeric_range(lower_limit = 0)
+
+	wflow <- workflow(rec_no_tuning, mod_tune_submodel, tlr_no_tune)
+
+	param_info <- get_param_info(wflow)
+
+	expect_named(param_info, c("name", "id", "source", "has_submodel"))
+	expect_identical(param_info$name, c("trees", "min_n"))
+	expect_identical(param_info$has_submodel, c(TRUE, FALSE))
+})
+
+
+# `schedule_predict_stage_i()` -------------------------------------------
+
 test_that("`schedule_predict_stage_i()` works with: no submodel, no post-processing", {
 	wflow <- workflow(mpg ~ ., mod_no_tune, tlr_no_tune)
 	param_info <- get_param_info(wflow)
@@ -55,60 +116,4 @@ test_that("`schedule_predict_stage_i()` works with: with submodel, with post-pro
 		schedule$post_stage[[2]],
 		tibble::tibble(lower_limit = 1:2)
 	)
-})
-
-test_that("`get_param_info()` works for a workflow without tags for tuning", {
-	wflow <- workflow(mpg ~ ., parsnip::linear_reg())
-	param_info <- get_param_info(wflow)
-
-	expect_named(param_info, c("name", "id", "source", "has_submodel"))
-	expect_identical(nrow(param_info), 0L)
-})
-
-test_that("`get_param_info()` works for a workflow with tags for tuning", {
-	# tuning tags in all components
-	rec_tune_2 <- recipes::recipe(mpg ~ ., data = mtcars) %>%
-		recipes::step_corr(recipes::all_predictors(), threshold = tune()) %>%
-		recipes::step_spline_natural(disp, deg_free = tune("disp_df"))
-
-	mod_tune_no_submodel <- parsnip::rand_forest(
-		min_n = tune(),
-		mode = "regression"
-	)
-
-	tlr_tune <- tailor::tailor() %>%
-		tailor::adjust_numeric_range(lower_limit = tune())
-
-	wflow <- workflow(rec_tune_2, mod_tune_no_submodel, tlr_tune)
-
-	param_info <- get_param_info(wflow)
-
-	expect_named(param_info, c("name", "id", "source", "has_submodel"))
-	expect_identical(
-		param_info$id,
-		c("min_n", "threshold", "disp_df", "lower_limit")
-	)
-	expect_identical(param_info$has_submodel, c(FALSE, NA, NA, NA))
-})
-
-test_that("`get_param_info()` works when there are submodel parameters", {
-	# tuning tags only in model spec
-	rec_no_tuning <- recipes::recipe(mpg ~ ., data = mtcars)
-
-	mod_tune_submodel <- parsnip::boost_tree(
-		trees = tune(),
-		min_n = tune(),
-		mode = "regression"
-	)
-
-	tlr_no_tune <- tailor::tailor() %>%
-		tailor::adjust_numeric_range(lower_limit = 0)
-
-	wflow <- workflow(rec_no_tuning, mod_tune_submodel, tlr_no_tune)
-
-	param_info <- get_param_info(wflow)
-
-	expect_named(param_info, c("name", "id", "source", "has_submodel"))
-	expect_identical(param_info$name, c("trees", "min_n"))
-	expect_identical(param_info$has_submodel, c(TRUE, FALSE))
 })
