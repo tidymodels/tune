91a19261b54cd8e58f4c03949e38d82aa485fa40
Hannah Frick <hannah@posit.co>
2025-02-26 17:14:59 +0000

    add tests for `schedule_predict_stage_i()`

--- tests/testthat/test-schedule.R
+++ tests/testthat/test-schedule.R
@@ -1,3 +1,62 @@
+
+test_that("`schedule_predict_stage_i()` works with: no submodel, no post-processing", {
+	wflow <- workflow(mpg ~ ., mod_no_tune, tlr_no_tune)
+	param_info <- get_param_info(wflow)
+	grid_predict_stage <- tibble::tibble()
+
+	schedule <- schedule_predict_stage_i(grid_predict_stage, param_info)
+	expect_named(schedule, c("post_stage"))
+	expect_identical(nrow(schedule), 0L)
+})
+
+test_that("`schedule_predict_stage_i()` works with: no submodel, with post-processing", {
+	wflow <- workflow(mpg ~ ., mod_no_tune, tlr_tune)
+	param_info <- get_param_info(wflow)
+	grid_predict_stage <- tibble::tibble(lower_limit = 1:2)
+
+	schedule <- schedule_predict_stage_i(grid_predict_stage, param_info)
+	expect_named(schedule, c("post_stage"))
+	expect_identical(nrow(schedule), 1L)
+	expect_identical(schedule$post_stage[[1]], grid_predict_stage)
+})
+
+test_that("`schedule_predict_stage_i()` works with: with submodel, no post-processing", {
+	wflow <- workflow(mpg ~ ., mod_tune_submodel, tlr_no_tune)
+	param_info <- get_param_info(wflow)
+	grid_predict_stage <- tibble::tibble(trees = 1:2)
+
+	schedule <- schedule_predict_stage_i(grid_predict_stage, param_info)
+	expect_named(schedule, c("trees", "post_stage"))
+	expect_identical(schedule$trees, grid_predict_stage$trees)
+	expect_identical(
+		purrr::map_int(schedule$post_stage, ncol),
+		c(0L, 0L)
+	)
+})
+
+test_that("`schedule_predict_stage_i()` works with: with submodel, with post-processing", {
+	wflow <- workflow(mpg ~ ., mod_tune_submodel, tlr_tune)
+	param_info <- get_param_info(wflow)
+	# semi-regular grid
+	grid_predict_stage <- list(
+		tibble::tibble(trees = 1L, lower_limit = 1L),
+		tibble::tibble(trees = c(2L, 2L), lower_limit = 1:2)
+	) %>%
+		purrr::list_rbind()
+
+	schedule <- schedule_predict_stage_i(grid_predict_stage, param_info)
+	expect_named(schedule, c("trees", "post_stage"))
+	expect_identical(schedule$trees, 1:2)
+	expect_identical(
+		schedule$post_stage[[1]],
+		tibble::tibble(lower_limit = 1L)
+	)
+	expect_identical(
+		schedule$post_stage[[2]],
+		tibble::tibble(lower_limit = 1:2)
+	)
+})
+
 test_that("`get_param_info()` works for a workflow without tags for tuning", {
 	wflow <- workflow(mpg ~ ., parsnip::linear_reg())
 	param_info <- get_param_info(wflow)
