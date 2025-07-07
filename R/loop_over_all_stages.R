a3f71945910beff21a19698c24f105736df711bb
Max Kuhn <mxkuhn@gmail.com>
2025-07-07 16:40:27 -0400

    Apply suggestions from code review
    Co-authored-by: Simon P. Couch <simonpatrickcouch@gmail.com>

--- R/loop_over_all_stages.R
+++ R/loop_over_all_stages.R
@@ -10,7 +10,7 @@ loop_over_all_stages <- function(resamples, grid, static) {
   # Set seed within the worker process
   assign(".Random.seed", resamples$.seeds[[1]], envir = .GlobalEnv)
   resamples$.seeds <- NULL
-
+  withr::defer(assign(".Random.seed", orig_seed, envir = .GlobalEnv))
   split <- resamples$splits[[1]]
   split_labs <- resamples |>
     dplyr::select(dplyr::starts_with("id"))
@@ -306,8 +306,6 @@ loop_over_all_stages <- function(resamples, grid, static) {
     ))
   }
 
-  # Put the seed back where we found it
-  assign(".Random.seed", orig_seed, envir = globalenv())
   return_tbl
 }
 
