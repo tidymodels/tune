fa56667742d8c2adde3537ac171256fbf8260ab4
Hannah Frick <hannah@posit.co>
2026-03-30 15:35:33 +0100

    `use_lifecycle()`

---
+++ R/tune-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom lifecycle deprecated
+## usethis namespace: end
+
+## mockable bindings: start
+## mockable bindings: end
+NULL
