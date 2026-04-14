139baeece7493631c132a98c2b7aa9480b39b425
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-04-14 15:11:20 -0700

    more specific guard for finalize_model()

--- R/finalize.R
+++ R/finalize.R
@@ -32,7 +32,7 @@
 finalize_model <- function(x, parameters) {
   if (!inherits(x, "model_spec") && !inherits(x, "cluster_spec")) {
     cli::cli_abort(
-      "{.arg x} should be a parsnip model specification, not
+      "{.arg x} should be a parsnip or tidymodels model specification, not
                     {.obj_type_friendly {x}}."
     )
   }
