948ce7e396ce4d83eb5091d2c12843a91121c194
Max Kuhn <mxkuhn@gmail.com>
2026-04-16 14:29:10 -0400

    remove line that was commented out

--- R/augment.R
+++ R/augment.R
@@ -113,6 +113,5 @@ merge_pred <- function(dat, pred, y) {
   if (all(pred_cols == ".pred")) {
     dat$.resid <- dat[[y]] - dat$.pred
   }
-  # dat <- dplyr::relocate(dat, dplyr::starts_with("."))
   reorder_pred_cols(dat, outcome = y, param = character(0))
 }
